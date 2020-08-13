41767e5e03fcd252916dc8b2a3074131a9b03b91
Andre Silva <andreqls@gmail.com>
2020-08-13 10:15:30 -0300

    Working with DOM manipulation

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,9 +1,25 @@
 var main = function () {
     "use strict";
 
+    /*
     $.getJSON("/counts.json", function (wordCounts) {
         console.log(wordCounts);
     });
+    */
+
+    var insertCountsIntoDOM = function (counts) {
+        var $block=$("<p>");
+        $block.val("");
+        $block.text("cool: "+counts.cool);
+        $(".content").append($block);
+    };
+
+    setInterval(function () {
+        $.getJSON("counts.json", function (counts) {
+            $(".content").empty();
+            insertCountsIntoDOM(counts);
+        });
+    }, 5000);
 
 };
 
