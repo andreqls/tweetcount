4cf6231188682db01e2f00edc5718a0a54dfdce2
Andre Silva <andreqls@gmail.com>
2020-08-13 08:58:47 -0300

    Working, without client

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express=require("express"),
     http=require("http"),
+    cookie=require("cookie"),
     tweetCounts=require("./tweet_counter.js"),
     app=express();
 
@@ -8,6 +9,8 @@ app.use(express.static(__dirname+"/client"));
 http.createServer(app).listen(3000);
 
 app.get("/counts.json", function(req,res) {
+    res.cookie("same-site-cookie","foo", {sameSite:"lax"});
+    res.cookie("cross-site-cookie","bar", {sameSite:"none", secure:"true"});
     res.json(tweetCounts);
 });
 
