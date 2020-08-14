545b14effe16e30443331ce04e07afa71878bc94
Andre Silva <andreqls@gmail.com>
2020-08-14 11:18:09 -0300

    Included basic Redis functionality, needs initialization from database

--- tweet_counter.js
+++ tweet_counter.js
@@ -1,4 +1,5 @@
 var ntwitter = require("ntwitter"),
+    redis = require("redis"),
     credentials = require("../credentials.json"),
     twitter,
     trackedWords = ["awesome","cool","rad","gnarly","groovy"],
@@ -7,6 +8,8 @@ var ntwitter = require("ntwitter"),
 twitter = ntwitter(credentials);
 trackedWords.forEach(function (word) { counts[word]=0; });
 
+redisClient = redis.createClient();
+
 twitter.stream(
     "statuses/filter",
 
@@ -14,21 +17,14 @@ twitter.stream(
 
     function(stream) {
         stream.on("data", function(tweet) {
-//          console.log(tweet.text);
             trackedWords.forEach(function (word) {
-                console.log(word+": "+tweet.text+"\n");
                 if (tweet.text.indexOf(word)>-1) {
+                    redisClient.incr(word);
                     counts[word]=counts[word]+1;
-                    console.log(word+" updated: now="+counts[word]+"\n");
                 }
             });
         });
     }
 );
-
-/*
-setInterval(function () {
-    console.log("cool: "+counts.cool);
-},3000);*/
 
 module.exports=counts;
